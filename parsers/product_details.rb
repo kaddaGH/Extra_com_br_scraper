16697a4543fecf8e875181c15ab09d7d294e1e9f
Kadda Ghouadni <ghouadni55@hotmail.fr>
2019-02-15 21:11:10 +0100

    Update scraper.

---
+++ parsers/product_details.rb
@@ -0,0 +1,78 @@
+data = JSON.parse(content)
+product = data['content']
+name= product['name']
+
+brand = (product['name'].downcase.include?'red bull')? 'Red Bull':nil
+if brand.nil?
+  brand =product['name'][/\s[A-Z\s]{4,}\s/]
+end
+category = data['categories'][0]['name']
+availability = product['stock'] == true ? '1' : ''
+pack = product['totalQuantity'].to_i == 0 ? '1' : product['totalQuantity'].to_i
+
+
+regexps = [
+    /(\d*[\.,]?\d+)\s?([Ff][Ll]\.?\s?[Oo][Zz])/,
+    /(\d*[\.,]?\d+)\s?([Oo][Zz])/,
+    /(\d*[\.,]?\d+)\s?([Ff][Oo])/,
+    /(\d*[\.,]?\d+)\s?([Ee][Aa])/,
+    /(\d*[\.,]?\d+)\s?([Ff][Zz])/,
+    /(\d*[\.,]?\d+)\s?(Fluid Ounces?)/,
+    /(\d*[\.,]?\d+)\s?([Oo]unce)/,
+    /(\d*[\.,]?\d+)\s?([Mm][Ll])/,
+    /(\d*[\.,]?\d+)\s?([Cc][Ll])/,
+    /(\d*[\.,]?\d+)\s?([Ll])/,
+    /(\d*[\.,]?\d+)\s?([Gg])/,
+    /(\d*[\.,]?\d+)\s?([Ll]itre)/,
+    /(\d*[\.,]?\d+)\s?([Ss]ervings)/,
+    /(\d*[\.,]?\d+)\s?([Pp]acket\(?s?\)?)/,
+    /(\d*[\.,]?\d+)\s?([Cc]apsules)/,
+    /(\d*[\.,]?\d+)\s?([Tt]ablets)/,
+    /(\d*[\.,]?\d+)\s?([Tt]ubes)/,
+    /(\d*[\.,]?\d+)\s?([Cc]hews)/,
+    /(\d*[\.,]?\d+)\s?([Mm]illiliter)/i,
+]
+regexps.find {|regexp| name =~ regexp}
+item_size = $1
+uom = $2
+
+
+
+product_details = {
+    # - - - - - - - - - - -
+    RETAILER_ID: '100',
+    RETAILER_NAME: 'deliveryextra',
+    GEOGRAPHY_NAME: 'BR',
+    # - - - - - - - - - - -
+    SCRAPE_INPUT_TYPE: page['vars']['input_type'],
+    SCRAPE_INPUT_SEARCH_TERM: page['vars']['search_term'],
+    SCRAPE_INPUT_CATEGORY: page['vars']['input_type'] == 'taxonomy' ? category : '-',
+    SCRAPE_URL_NBR_PRODUCTS: scrape_url_nbr_products,
+    # - - - - - - - - - - -
+    SCRAPE_URL_NBR_PROD_PG1: nbr_products_pg1,
+    # - - - - - - - - - - -
+    PRODUCT_BRAND: brand,
+    PRODUCT_RANK: i + 1,
+    PRODUCT_PAGE: current_page + 1,
+    PRODUCT_ID: product['id'],
+    PRODUCT_NAME: product['name'],
+    PRODUCT_DESCRIPTION: "",
+    PRODUCT_MAIN_IMAGE_URL: "https://www.deliveryextra.com.br"+product['thumbPath'].gsub(/50x50/,'200x200'),
+    PRODUCT_ITEM_SIZE: item_size,
+    PRODUCT_ITEM_SIZE_UOM: uom,
+    PRODUCT_ITEM_QTY_IN_PACK: pack,
+    SALES_PRICE: product['currentPrice'],
+    IS_AVAILABLE: availability,
+    PROMOTION_TEXT: "",
+}
+
+pages << {
+    page_type: 'product_reviews',
+    method: 'GET',
+    url: "https://api.gpa.digital/ex/products/#{product['id']}/review?&searchkeyword=#{page['vars']['search_term']}&searchpage=#{page['vars']['page']}",
+    vars: {
+        'product_details' => product_details
+    }
+
+
+}
--- parsers/products_search.rb
+++ parsers/products_search.rb
@@ -1,20 +1,30 @@
 data = JSON.parse(content)
 
-data = data['content']
-scrape_url_nbr_products = data['totalElements'].to_i
-current_page = data['number'].to_i
-page_size = data['size'].to_i
-number_of_pages = data['totalPages'].to_i
-products = data['products']
+
+if data.key?('content')
+  data = data['content']
+  scrape_url_nbr_products = data['totalElements'].to_i
+  current_page = data['number'].to_i
+  page_size = data['size'].to_i
+  number_of_pages = data['totalPages'].to_i
+  products = data['products']
+
+else
+  scrape_url_nbr_products = data['result_meta']['total'].to_i
+  current_page = data['pages']['current']['name'].to_i-1
+  page_size = data['result_meta']['this_page'].to_i
+  number_of_pages = data['pages']['total'].to_i
+  products = data['results']
+end
 
 
-# if ot's first page , generate pagination
+# if it's first page , generate pagination
 if current_page == 0 and number_of_pages > 1
   nbr_products_pg1 = products.length
   step_page = 1
   while step_page < number_of_pages
     url = page['url'].gsub(/p=0/, "p=#{step_page}")
-    url = url.gsub(/\&srt=00/, "\&srt=#{step_page}*page_size")
+    url = url.gsub(/\&srt=0/, "\&srt=#{step_page*page_size}")
 
     pages << {
         page_type: 'products_search',
@@ -41,85 +51,29 @@ end
 
 products.each_with_index do |product, i|
 
+if product.key?('id')
 
+product_id = product['id']
+else
 
-  name= product['name']
+  product_id = product['url'][/\d+?\Z/]
 
-  brand = (product['name'].downcase.include?'red bull')? 'Red Bull':nil
-  if brand.nil?
-    brand =product['name'][/\s[A-Z\s]{4,}\s/]
-  end
-  category = data['categories'][0]['name']
-  availability = product['stock'] == true ? '1' : ''
-  pack = product['totalQuantity'].to_i == 0 ? '1' : product['totalQuantity'].to_i
-
-
-  regexps = [
-      /(\d*[\.,]?\d+)\s?([Ff][Ll]\.?\s?[Oo][Zz])/,
-      /(\d*[\.,]?\d+)\s?([Oo][Zz])/,
-      /(\d*[\.,]?\d+)\s?([Ff][Oo])/,
-      /(\d*[\.,]?\d+)\s?([Ee][Aa])/,
-      /(\d*[\.,]?\d+)\s?([Ff][Zz])/,
-      /(\d*[\.,]?\d+)\s?(Fluid Ounces?)/,
-      /(\d*[\.,]?\d+)\s?([Oo]unce)/,
-      /(\d*[\.,]?\d+)\s?([Mm][Ll])/,
-      /(\d*[\.,]?\d+)\s?([Cc][Ll])/,
-      /(\d*[\.,]?\d+)\s?([Ll])/,
-      /(\d*[\.,]?\d+)\s?([Gg])/,
-      /(\d*[\.,]?\d+)\s?([Ll]itre)/,
-      /(\d*[\.,]?\d+)\s?([Ss]ervings)/,
-      /(\d*[\.,]?\d+)\s?([Pp]acket\(?s?\)?)/,
-      /(\d*[\.,]?\d+)\s?([Cc]apsules)/,
-      /(\d*[\.,]?\d+)\s?([Tt]ablets)/,
-      /(\d*[\.,]?\d+)\s?([Tt]ubes)/,
-      /(\d*[\.,]?\d+)\s?([Cc]hews)/,
-      /(\d*[\.,]?\d+)\s?([Mm]illiliter)/i,
-  ]
-  regexps.find {|regexp| name =~ regexp}
-  item_size = $1
-  uom = $2
-
-
-
-  product_details = {
-      # - - - - - - - - - - -
-      RETAILER_ID: '100',
-      RETAILER_NAME: 'deliveryextra',
-      GEOGRAPHY_NAME: 'BR',
-      # - - - - - - - - - - -
-      SCRAPE_INPUT_TYPE: page['vars']['input_type'],
-      SCRAPE_INPUT_SEARCH_TERM: page['vars']['search_term'],
-      SCRAPE_INPUT_CATEGORY: page['vars']['input_type'] == 'taxonomy' ? category : '-',
-      SCRAPE_URL_NBR_PRODUCTS: scrape_url_nbr_products,
-      # - - - - - - - - - - -
-      SCRAPE_URL_NBR_PROD_PG1: nbr_products_pg1,
-      # - - - - - - - - - - -
-      PRODUCT_BRAND: brand,
-      PRODUCT_RANK: i + 1,
-      PRODUCT_PAGE: current_page + 1,
-      PRODUCT_ID: product['id'],
-      PRODUCT_NAME: product['name'],
-      PRODUCT_DESCRIPTION: "",
-      PRODUCT_MAIN_IMAGE_URL: "https://www.deliveryextra.com.br"+product['thumbPath'].gsub(/50x50/,'200x200'),
-      PRODUCT_ITEM_SIZE: item_size,
-      PRODUCT_ITEM_SIZE_UOM: uom,
-      PRODUCT_ITEM_QTY_IN_PACK: pack,
-      SALES_PRICE: product['currentPrice'],
-      IS_AVAILABLE: availability,
-      PROMOTION_TEXT: "",
-  }
+end
 
   pages << {
-      page_type: 'product_reviews',
+      page_type: 'product_details',
       method: 'GET',
-      url: "https://api.gpa.digital/ex/products/#{product['id']}/review?&searchkeyword=#{page['vars']['search_term']}&searchpage=#{page['vars']['page']}",
+      url: "https://api.gpa.digital/ex/v3/products/ecom/#{product_id}?storeId=241&isClienteMais=false&searchkeyword=#{page['vars']['search_term']}&searchpage=#{page['vars']['page']}",
       vars: {
-          'product_details' => product_details
+          'input_type' => page['vars']['input_type'],
+          'search_term' => page['vars']['search_term'],
+          'page' => page['vars']['page'],
+          'nbr_products_pg1' => nbr_products_pg1,
+          'rank' => i+1
       }
 
 
   }
-
 end
 
 
